70722ed7330aea3b928664f53efde1e763482a27
Tyler Barnes <tyler@bare.ca>
2019-01-06 12:28:58 -0800

    move footer from layout to pages

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,17 +1,20 @@
 import React from 'react'
 
-import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { Footer } from '../components/footer'
 
-import AniLink from "gatsby-plugin-transition-link/AniLink";
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
 const SecondPage = () => (
-    <div>
-      <SEO title="Page two" />
-      <h1 style={{padding: 200 + 'px'}}>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
-      <AniLink fade to="/">Go back to the homepage</AniLink>
-    </div>
+  <>
+    <SEO title="Page two" />
+    <h1 style={{ padding: 200 + 'px' }}>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <AniLink fade to="/">
+      Go back to the homepage
+    </AniLink>
+    <Footer />
+  </>
 )
 
 export default SecondPage
